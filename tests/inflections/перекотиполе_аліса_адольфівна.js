e191071cf43c6db14a9db1904cfbd69c5ddf3d8c
tooleks <tooleks@gmail.com>
2017-08-06 22:00:54 +0300

    Added test

---
+++ tests/inflections/перекотиполе_аліса_адольфівна.js
@@ -0,0 +1,53 @@
+var assert = require("assert");
+var testInflections = require("../helpers").testInflections;
+
+describe("inflection перекотиполе аліса адольфівна", function () {
+
+    var person = {
+        gender: "female",
+        lastName: "перекотиполе",
+        firstName: "аліса",
+        middleName: "адольфівна"
+    };
+
+    var validResults = {
+        nominative: {
+            lastName: "перекотиполе",
+            firstName: "аліса",
+            middleName: "адольфівна"
+        },
+        genitive: {
+            lastName: "перекотиполя",
+            firstName: "аліси",
+            middleName: "адольфівни"
+        },
+        dative: {
+            lastName: "перекотиполю",
+            firstName: "алісі",
+            middleName: "адольфівні"
+        },
+        accusative: {
+            lastName: "перекотиполе",
+            firstName: "алісу",
+            middleName: "адольфівну"
+        },
+        ablative: {
+            lastName: "перекотиполем",
+            firstName: "алісою",
+            middleName: "адольфівною"
+        },
+        locative: {
+            lastName: "перекотиполі",
+            firstName: "алісі",
+            middleName: "адольфівні"
+        },
+        vocative: {
+            lastName: "перекотиполе",
+            firstName: "алісо",
+            middleName: "адольфівно"
+        }
+    };
+
+    testInflections(person, validResults);
+
+});
