58228f3e2cca72c15b7389ac86246991985ecc07
tooleks <tooleks@gmail.com>
2017-08-08 22:04:58 +0300

    Updated rules, added tests

---
+++ tests/inflections/кузик_андрій_іванович.js
@@ -0,0 +1,49 @@
+var assert = require("assert");
+var testInflections = require("../helpers").testInflections;
+
+var person = {
+    gender: "male",
+    lastName: "кузик",
+    firstName: "андрій",
+    middleName: "іванович"
+};
+
+var validResults = {
+    nominative: {
+        lastName: "кузик",
+        firstName: "андрій",
+        middleName: "іванович"
+    },
+    genitive: {
+        lastName: "кузика",
+        firstName: "андрія",
+        middleName: "івановича"
+    },
+    dative: {
+        lastName: "кузику",
+        firstName: "андрію",
+        middleName: "івановичу"
+    },
+    accusative: {
+        lastName: "кузика",
+        firstName: "андрія",
+        middleName: "івановича"
+    },
+    ablative: {
+        lastName: "кузиком",
+        firstName: "андрієм",
+        middleName: "івановичем"
+    },
+    locative: {
+        lastName: "кузикові",
+        firstName: "андрієві",
+        middleName: "івановичу"
+    },
+    vocative: {
+        lastName: "кузику",
+        firstName: "андрію",
+        middleName: "івановичу"
+    }
+};
+
+testInflections(person, validResults);
