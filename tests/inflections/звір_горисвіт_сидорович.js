1e33e1534735a0b3abcc97812cbd1d8e2bb61c15
tooleks <tooleks@gmail.com>
2017-08-13 19:16:38 +0300

    Updated rules, added tests

---
+++ tests/inflections/звір_горисвіт_сидорович.js
@@ -0,0 +1,49 @@
+const assert = require("assert");
+const testInflections = require("../helpers").testInflections;
+
+const person = {
+    gender: "male",
+    lastName: "звір",
+    firstName: "горисвіт",
+    middleName: "сидорович"
+};
+
+const validResults = {
+    nominative: {
+        lastName: "звір",
+        firstName: "горисвіт",
+        middleName: "сидорович"
+    },
+    genitive: {
+        lastName: "звіра",
+        firstName: "горисвіта",
+        middleName: "сидоровича"
+    },
+    dative: {
+        lastName: "звіру",
+        firstName: "горисвіту",
+        middleName: "сидоровичу"
+    },
+    accusative: {
+        lastName: "звіра",
+        firstName: "горисвіта",
+        middleName: "сидоровича"
+    },
+    ablative: {
+        lastName: "звіром",
+        firstName: "горисвітом",
+        middleName: "сидоровичем"
+    },
+    locative: {
+        lastName: "звірі",
+        firstName: "горисвітові",
+        middleName: "сидоровичу"
+    },
+    vocative: {
+        lastName: "звіре",
+        firstName: "горисвіте",
+        middleName: "сидоровичу"
+    }
+};
+
+testInflections(person, validResults);
---
+++ tests/inflections/кріль_дій_олегович.js
@@ -0,0 +1,49 @@
+const assert = require("assert");
+const testInflections = require("../helpers").testInflections;
+
+const person = {
+    gender: "male",
+    lastName: "кріль",
+    firstName: "дій",
+    middleName: "олегович"
+};
+
+const validResults = {
+    nominative: {
+        lastName: "кріль",
+        firstName: "дій",
+        middleName: "олегович"
+    },
+    genitive: {
+        lastName: "кроля",
+        firstName: "дія",
+        middleName: "олеговича"
+    },
+    dative: {
+        lastName: "кролю",
+        firstName: "дію",
+        middleName: "олеговичу"
+    },
+    accusative: {
+        lastName: "кроля",
+        firstName: "дія",
+        middleName: "олеговича"
+    },
+    ablative: {
+        lastName: "кролем",
+        firstName: "дієм",
+        middleName: "олеговичем"
+    },
+    locative: {
+        lastName: "кролю",
+        firstName: "дієві",
+        middleName: "олеговичу"
+    },
+    vocative: {
+        lastName: "кролю",
+        firstName: "дію",
+        middleName: "олеговичу"
+    }
+};
+
+testInflections(person, validResults);
