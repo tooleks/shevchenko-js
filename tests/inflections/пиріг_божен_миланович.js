732df065b3e56dbafe09daf7f8e1f55047678610
tooleks <tooleks@gmail.com>
2017-08-14 12:30:22 +0300

    Updated rules, added tests

---
+++ tests/inflections/пиріг_божен_миланович.js
@@ -0,0 +1,49 @@
+const assert = require("assert");
+const testInflections = require("../helpers").testInflections;
+
+const person = {
+    gender: "male",
+    lastName: "пиріг",
+    firstName: "божен",
+    middleName: "миланович"
+};
+
+const validResults = {
+    nominative: {
+        lastName: "пиріг",
+        firstName: "божен",
+        middleName: "миланович"
+    },
+    genitive: {
+        lastName: "пирога",
+        firstName: "божена",
+        middleName: "милановича"
+    },
+    dative: {
+        lastName: "пирогу",
+        firstName: "божену",
+        middleName: "милановичу"
+    },
+    accusative: {
+        lastName: "пирога",
+        firstName: "божена",
+        middleName: "милановича"
+    },
+    ablative: {
+        lastName: "пирогом",
+        firstName: "боженом",
+        middleName: "милановичем"
+    },
+    locative: {
+        lastName: "пирозі",
+        firstName: "боженові",
+        middleName: "милановичу"
+    },
+    vocative: {
+        lastName: "пирогу",
+        firstName: "божене",
+        middleName: "милановичу"
+    }
+};
+
+testInflections(person, validResults);
