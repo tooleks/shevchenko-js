e1e52d2c77e2aa93b377315e4cc79316859509e0
tooleks <tooleks@gmail.com>
2017-08-17 19:01:31 +0300

    Updated rules, added tests

---
+++ tests/inflections/кожух_данило_гнатович.js
@@ -0,0 +1,49 @@
+const assert = require("assert");
+const testInflections = require("../helpers").testInflections;
+
+const person = {
+    gender: "male",
+    lastName: "кожух",
+    firstName: "данило",
+    middleName: "гнатович"
+};
+
+const validResults = {
+    nominative: {
+        lastName: "кожух",
+        firstName: "данило",
+        middleName: "гнатович"
+    },
+    genitive: {
+        lastName: "кожуха",
+        firstName: "данила",
+        middleName: "гнатовича"
+    },
+    dative: {
+        lastName: "кожуху",
+        firstName: "данилу",
+        middleName: "гнатовичу"
+    },
+    accusative: {
+        lastName: "кожуха",
+        firstName: "данила",
+        middleName: "гнатовича"
+    },
+    ablative: {
+        lastName: "кожухом",
+        firstName: "данилом",
+        middleName: "гнатовичем"
+    },
+    locative: {
+        lastName: "кожухові",
+        firstName: "данилові",
+        middleName: "гнатовичу"
+    },
+    vocative: {
+        lastName: "кожуху",
+        firstName: "даниле",
+        middleName: "гнатовичу"
+    }
+};
+
+testInflections(person, validResults);
