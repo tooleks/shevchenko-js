5e26d55190df21d32c4758a5a1d639ab908708a0
tooleks <tooleks@gmail.com>
2017-08-27 22:40:04 +0300

    Refactored the pos neural network learning rate test

--- tests/pos-neural-network-learning-rate-test.js
+++ tests/pos-neural-network-learning-rate-test.js
@@ -5,13 +5,13 @@ const pos = require("../dist/module/pos");
 const NeuralNetwork = require("../dist/module/pos/neural-network");
 const samples = require("../neural-networks/pos/data/training-data.json");
 
-describe("#pos.NeuralNetwork", () => {
-    it("should have learn rate value >= 0.95", () => {
+describe("#pos.NeuralNetwork learning rate", () => {
+    it("should have learning rate value >= 0.95", () => {
         const results = samples
             .filter((sample) => NeuralNetwork.isValidPosName(sample.pos))
             .map((sample) => pos.resolve(sample.value, false) === sample.pos);
         const correctResults = results.filter((result) => result);
-        const learnRate = correctResults.length / results.length;
-        assert(learnRate >= 0.95, `The learn rate value is too low: ${learnRate}.`);
+        const learningRate = correctResults.length / results.length;
+        assert(learningRate >= 0.95, `The learn rate value is too low: ${learningRate}.`);
     });
 });
