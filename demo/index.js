e3414952d6eb45215acac596b3a2dabe3662c8c3
tooleks <tooleks@gmail.com>
2017-10-24 18:44:18 +0300

    Implemented a backend for the demo page

---
+++ demo/index.js
@@ -0,0 +1,23 @@
+require("dotenv").config();
+
+const express = require("express");
+const app = express();
+
+const port = process.env.HTTP_PORT || 8080;
+
+app.use(express.static("public"));
+app.use(express.static("public/meta"));
+
+app.engine("html", require("ejs").renderFile);
+app.set("view engine", "html");
+
+app.get("/", (req, res) => {
+    res.render("layout.ejs", {
+        env: process.env,
+        message: "Hello there!"
+    })
+});
+
+app.listen(port, () => {
+    console.log(`Listening on port ${port}!`);
+});
