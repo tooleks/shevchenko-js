bf431c498958df53afe11e95c42faff40a6bc416
tooleks <tooleks@gmail.com>
2017-10-27 20:46:12 +0300

    Fixed links

--- demo/app/services/social/share-links-provider.js
+++ demo/app/services/social/share-links-provider.js
@@ -2,9 +2,9 @@
 
 const qs = require("querystring");
 
-const facebook = (url, description) => `https://www.facebook.com/sharer/sharer.php?u=${url}`;
-const twitter = (url, description) => `https://twitter.com/home?status=${url}`;
-const googlePlus = (url, description) => `https://plus.google.com/share?url=${url}`;
-const linkedIn = (url, description) => `https://www.linkedin.com/shareArticle?mini=true&url=${url}&title=&summary=${description}&source=`;
+const facebook = (url, description) => `https://www.facebook.com/sharer/sharer.php?u=${qs.escape(url)}`;
+const twitter = (url, description) => `https://twitter.com/home?status=${qs.escape(url)}`;
+const googlePlus = (url, description) => `https://plus.google.com/share?url=${qs.escape(url)}`;
+const linkedIn = (url, description) => `https://www.linkedin.com/shareArticle?mini=true&url=${qs.escape(url)}&title=&summary=${qs.escape(description)}&source=`;
 
 module.exports = {facebook, twitter, googlePlus, linkedIn};
