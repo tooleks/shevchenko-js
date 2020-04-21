6acbc6edb83abc71e500c44d8eef7232b5de45de
tooleks <tooleks@gmail.com>
2020-04-21 11:52:11 +0300

    Removed unused members from internal namespace

--- src-ts/internal.ts
+++ src-ts/internal.ts
@@ -1,5 +1,3 @@
-import WordEncoder from './PartOfSpeechRecognizer/WordEncoder';
-import PartOfSpeechEncoder from './PartOfSpeechRecognizer/PartOfSpeechEncoder';
 import NeuralNetwork from './PartOfSpeechRecognizer/NeuralNetwork';
 
-export { WordEncoder, PartOfSpeechEncoder, NeuralNetwork };
+export { NeuralNetwork };
