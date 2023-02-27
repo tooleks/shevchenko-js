18f891853b77006ef42798df41f6014de0fccbf1
tooleks <tooleks@gmail.com>
2023-02-27 19:42:54 +0200

    feat(site): display the current library version

--- site/components/page-footer.vue
+++ site/components/page-footer.vue
@@ -3,30 +3,37 @@ const appConfig = useAppConfig();
 </script>
 
 <template>
-  <div class="row">
+  <hr />
+
+  <div class="row mb-1">
     <div class="col-12">
-      <hr />
-      <nav>
-        <ul class="list-unstyled" role="menubar">
-          <p class="d-inline mr-2 mb-0">
-            <PageCopyright />
-          </p>
+      <ul class="list-unstyled mb-0" role="menubar">
+        <li class="d-inline mr-2" role="presentation">
+          <span class="text-muted">
+            {{ $t('library.version', { version: appConfig.library.version }) }}
+          </span>
+        </li>
 
-          <li class="d-inline mr-2" role="presentation">
-            <a :href="appConfig.library.gitHubUrl" target="_blank" role="menuitem">GitHub</a>
-          </li>
+        <li class="d-inline mr-2" role="presentation">
+          <a :href="appConfig.library.gitHubUrl" target="_blank" role="menuitem">GitHub</a>
+        </li>
 
-          <li class="d-inline mr-2" role="presentation">
-            <a :href="appConfig.library.npmUrl" target="_blank" role="menuitem">NPM</a>
-          </li>
+        <li class="d-inline mr-2" role="presentation">
+          <a :href="appConfig.library.npmUrl" target="_blank" role="menuitem">NPM</a>
+        </li>
 
-          <li class="d-inline mr-2" role="presentation">
-            <ContactMeButton role="menuitem">
-              {{ $t('action.contactMe') }}
-            </ContactMeButton>
-          </li>
-        </ul>
-      </nav>
+        <li class="d-inline mr-2" role="presentation">
+          <ContactMeButton role="menuitem">
+            {{ $t('action.contactMe') }}
+          </ContactMeButton>
+        </li>
+      </ul>
+    </div>
+  </div>
+
+  <div class="row mb-3">
+    <div class="col-12">
+      <PageCopyright />
     </div>
   </div>
 </template>
