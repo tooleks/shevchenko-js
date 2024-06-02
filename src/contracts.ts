96468fcf404b1ed0089184a8a63d6d6d1e2da5e0
tooleks <tooleks@gmail.com>
2024-06-02 16:04:15 +0300

    docs: add examples to public types

--- src/contracts.ts
+++ src/contracts.ts
@@ -2,6 +2,16 @@ import { GrammaticalGender } from './language';
 
 /**
  * The input parameters for anthroponym declension.
+ *
+ * @example
+ * ```ts
+ * {
+ *   gender: shevchenko.GrammaticalGender.MASCULINE,
+ *   givenName: 'Тарас',
+ *   patronymicName: 'Григорович',
+ *   familyName: 'Шевченко',
+ * }
+ * ```
  */
 export type DeclensionInput = {
   gender: GrammaticalGender;
@@ -12,11 +22,29 @@ export type DeclensionInput = {
 
 /**
  * The output result of anthroponym declension.
+ *
+ * @example
+ * ```ts
+ * {
+ *   givenName: 'Тарасе',
+ *   patronymicName: 'Григоровичу',
+ *   familyName: 'Шевченку',
+ * }
+ * ```
  */
 export type DeclensionOutput<T extends DeclensionInput = DeclensionInput> = Omit<T, 'gender'>;
 
 /**
  * The input parameters for gender detection.
+ *
+ * @example
+ * ```ts
+ * {
+ *   givenName: 'Тарас',
+ *   patronymicName: 'Григорович',
+ *   familyName: 'Шевченко',
+ * }
+ * ```
  */
 export type GenderDetectionInput = {
   givenName?: string;
@@ -26,5 +54,12 @@ export type GenderDetectionInput = {
 
 /**
  * The output result of gender detection.
+ *
+ * @example
+ * ```ts
+ * {
+ *   gender: 'masculine',
+ * }
+ * ```
  */
 export type GenderDetectionOutput = GrammaticalGender | null;
