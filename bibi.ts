09399359db9d26f682352552c681a2a754519fd2
tooleks <tooleks@users.noreply.github.com>
2025-08-25 23:23:13 +0300

    chore: rm tmp file

--- bibi.ts
+++
@@ -1,9 +0,0 @@
-import * as shevchenko from './src/shevchenko';
-
-async function run() {
-  // @ts-ignore
-  const result = await shevchenko.inGenitive({ gender: 'masculine', familyName: 'Дятел' });
-  console.log(result);
-}
-
-run();
